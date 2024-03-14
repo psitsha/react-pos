37b6229d77f88044678dae3477c96b80f944499a
psitsha <sitsha.polite@yahoo.ie>
2024-03-14 15:29:58 +0000

    Remove from cart/ Calc Total Mount

--- frontend/src/pages/POSpage.jsx
+++ frontend/src/pages/POSpage.jsx
@@ -7,6 +7,7 @@ function POSpage() {
     const [products, setProducts] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [cart, setCart] = useState([]);
+    const [totalAmount, setTotalAmount] = useState(0);
     
     const fetchProducts = async() => {
       setIsLoading(true);
@@ -34,7 +35,7 @@ function POSpage() {
                 }
               newCart.push(newItem);
               } else {
-                newCart.push(newItem);
+                newCart.push(cartItem);
               }
             });
 
@@ -49,10 +50,24 @@ function POSpage() {
             setCart([...cart, addingProduct]);
           }
       }
+
+      const removeProduct = async(product) => {
+        // filter the product out of the cart
+        const newCart = cart.filter(cartItem => cartItem.id !== product.id);
+        setCart(newCart);               
+      }
     
       useEffect(() => {
         fetchProducts();
       },[]);
+
+      useEffect(() => {
+        let newTotalAmount = 0;
+        cart.forEach(icart => {
+          newTotalAmount = newTotalAmount + parseInt(icart.totalAmount);
+        })
+        setTotalAmount(newTotalAmount);
+      }, [cart])
 
       useEffect(() => {
         console.log(products);
@@ -75,7 +90,38 @@ function POSpage() {
                 </div>}
                 
               </div>
-              <div className='col-lg-4'></div>
+              <div className='col-lg-4'>
+                <div className='table-responsive bg-dark'>
+                  <table className='table table-responsive table-dark table-hover'>
+                    <thead>
+                      <tr>
+                        <td>#</td>
+                        <td>Name</td>
+                        <td>Price</td>
+                        <td>Quantity</td>
+                        <td>Total</td>
+                        <td>Action</td>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        cart ? cart.map((cartProduct, key) => <tr key={key}>
+                          <td>{cartProduct.id}</td>
+                          <td>{cartProduct.name}</td>                          
+                          <td>{cartProduct.price}</td>                          
+                          <td>{cartProduct.quantity}</td>                          
+                          <td>{cartProduct.totalAmount}</td>                          
+                          <td>
+                            <button className='btn btn-danger btn-sm' onClick={() => removeProduct(cartProduct)}>Remove</button>
+                          </td>                                                    
+                        </tr>)
+                        : 'No item in Cart'
+                      }                                            
+                    </tbody>
+                  </table>
+                  <h2 className='px-2 text-white'>Total Amount: €{totalAmount}</h2>
+                </div>
+              </div>
             </div>
         </MainLayout>
     )
