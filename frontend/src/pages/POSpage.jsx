c92a0c228270eed629d5fbe7ab2ceeb219122559
psitsha <sitsha.polite@yahoo.ie>
2024-03-14 12:31:08 +0000

    Add product to cart

--- frontend/src/pages/POSpage.jsx
+++ frontend/src/pages/POSpage.jsx
@@ -6,12 +6,48 @@ function POSpage() {
 
     const [products, setProducts] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [cart, setCart] = useState([]);
     
     const fetchProducts = async() => {
       setIsLoading(true);
         const result = await axios.get('products');
         setProducts(await result.data);
         setIsLoading(false);
+      }
+
+      const addProductToCart = async(product) =>{
+          // check if clicked product exist
+          let findProductInCart = await cart.find(i=>{
+            return i.id === product.id
+          });
+
+          if(findProductInCart){
+            let newCart = [];
+            let newItem;
+
+            cart.forEach(cartItem => {
+              if(cartItem.id === product.id){
+                newItem = {
+                  ...cartItem,
+                  quantity: cartItem.quantity + 1,
+                  totalAmount: cartItem.price * (cartItem.quantity + 1)
+                }
+              newCart.push(newItem);
+              } else {
+                newCart.push(newItem);
+              }
+            });
+
+            setCart(newCart);
+
+          } else {
+            let addingProduct = {
+              ...product,
+              'quantity': 1,
+              'totalAmount': product.price,
+            }
+            setCart([...cart, addingProduct]);
+          }
       }
     
       useEffect(() => {
@@ -29,7 +65,7 @@ function POSpage() {
                 {isLoading ? 'Loading...' : <div className='row'>
                   {products.map((product, key) =>
                     <div key={key} className='col-lg-4'>
-                      <div className='border'>
+                      <div className='border' onClick={() => addProductToCart(product)}>
                         <p>{product.name}</p>
                         <img src={product.image} className='img-fluid' alt={product.name} />
                         <p>€{product.price}</p>
