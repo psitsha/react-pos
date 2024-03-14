647e5926f4ba82432e2db37ee0ceed486a104003
psitsha <sitsha.polite@yahoo.ie>
2024-03-14 02:09:48 +0000

    get data using axios from backend

--- frontend/src/pages/POSpage.jsx
+++ frontend/src/pages/POSpage.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MainLayout from '../layouts/MainLayout'
+import axios from "axios"
 
 function POSpage() {
+
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async() => {
+        const result = await axios.get('products');
+        setProducts(await result.data);
+      }
+    
+      useEffect(() => {
+        fetchProducts();
+      },[]);
+
+      useEffect(() => {
+        console.log(products);
+      },[products]);
+
     return (
-        <MainLayout>POSpage</MainLayout>
+        <MainLayout>
+            POSpage
+        </MainLayout>
     )
 }
 
