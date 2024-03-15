f2466ea70bdeac36e0aa3d72e574f58dadc8a21d
psitsha <sitsha.polite@yahoo.ie>
2024-03-15 20:06:14 +0000

    Tidy up CSS

--- frontend/src/pages/POSpage.jsx
+++ frontend/src/pages/POSpage.jsx
@@ -95,8 +95,8 @@ function POSpage() {
               <div className='col-lg-8'>
                 {isLoading ? 'Loading...' : <div className='row'>
                   {products.map((product, key) =>
-                    <div key={key} className='col-lg-4'>
-                      <div className='border' onClick={() => addProductToCart(product)}>
+                    <div key={key} className='col-lg-4 mb-4'>
+                      <div className='pos-item px-3 text-center border' onClick={() => addProductToCart(product)}>
                         <p>{product.name}</p>
                         <img src={product.image} className='img-fluid' alt={product.name} />
                         <p>€{product.price}</p>
